5142b88a7e70dc7814f4918bd6a5c06bfcc94934
Oshan Kottege <oshan.kottege@myob.com>
2019-01-15 10:28:22 +1300

    Added the rest of the initial structure.

---
+++ app/index.js
@@ -0,0 +1,1 @@
+import React from 'react';import ReactDOM from 'react-dom';import 'index.css';class App extends React.Component {    render() {        return (            <div>Hello world!</div>        );    }}ReactDOM.render(<App/>, document.getElementById('app'));
