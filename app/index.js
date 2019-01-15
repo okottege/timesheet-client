46b3996672d4ff9a2af2b9eca00a44af80a81c86
Oshan Kottege <oshan.kottege@gmail.com>
2019-01-15 22:23:30 +1300

    Got the hot realoading working.

--- app/index.js
+++ app/index.js
@@ -1,1 +1,1 @@
-import React from 'react';import ReactDOM from 'react-dom';import './index.css';class App extends React.Component {    render() {        return (            <div>Hello world!</div>        );    }}ReactDOM.render(<App/>, document.getElementById('app'));
+import React from 'react';import ReactDOM from 'react-dom';import './index.css';class App extends React.Component {    render() {        return (            <div>Hello Mr Developer!</div>        );    }}ReactDOM.render(<App/>, document.getElementById('app'));
