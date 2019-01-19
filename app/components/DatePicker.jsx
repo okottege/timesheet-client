60fd90ba4b7e6ce152015df1b2124c068730e72a
Oshan Kottege <oshan.kottege@gmail.com>
2019-01-19 22:33:24 +1300

    Added few more tests to the DatePicker component.

--- app/components/DatePicker.jsx
+++ app/components/DatePicker.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class DatePicker extends React.Component {
+  hasTitle() {
+    return !!this.props.title;
+  }
   render() {
     return (
-      <div>Date Picker</div>
+      <div>
+        {this.hasTitle() &&
+          <span>{this.props.title}</span>
+        }
+      </div>
     );
   }
 }
+
+DatePicker.propTypes = {
+  title: PropTypes.string,
+  selectedDate: PropTypes.instanceOf(Date),
+  dateRequired: PropTypes.bool,
+  defaultToCurrentDate: PropTypes.bool
+};
 
 export default DatePicker;
--- app/components/__tests__/DatePickerTests.jsx
+++ app/components/__tests__/DatePickerTests.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import DatePicker from '../DatePicker';
 
-
 describe('DatePicker functionality', () => {
   test('DatePicker renders without crashing', () => {
     const datePicker = shallow(<DatePicker/>);
     expect(datePicker.length).toEqual(1);
+  });
+  test('DatePicker renders with title', () =>{
+    const datePicker = shallow(<DatePicker title='Date of birth' />);
+    expect(datePicker.find('span').text()).toBe('Date of birth');
+  });
+  test('If no title is not specified no span element exists', () => {
+    const datePicker = shallow(<DatePicker title='' />);
+    expect(datePicker.find('span').length).toEqual(0);
   });
 });
