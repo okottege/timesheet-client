8e9fe797f5c156ab4a9c1f880cd75bfa2860decc
Oshan Kottege <oshan.kottege@gmail.com>
2019-01-21 18:39:24 +1300

    Added test cases for the month dropdown component.

--- app/components/MonthDropdown.js
+++ app/components/MonthDropdown.js
@@ -2,11 +2,35 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class MonthDropdown extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      months: [
+        {name: 'January', value: 0},
+        {name: 'February', value: 1},
+        {name: 'March', value: 2},
+        {name: 'April', value: 3},
+        {name: 'May', value: 4},
+        {name: 'June', value: 5},
+        {name: 'July', value: 6},
+        {name: 'August', value: 7},
+        {name: 'September', value: 8},
+        {name: 'October', value: 9},
+        {name: 'November', value: 10},
+        {name: 'December', value: 11},
+      ]
+    }
+  }
+  renderMonths() {
+    return this.state.months.map(m => (
+      <option key={m.value} value={m.value}>{m.name}</option>
+    ));
+  }
   render() {
     return (
       <div>
         <select>
-
+          {this.renderMonths()}
         </select>
       </div>
     );
--- app/components/__tests__/MonthDropdown.js
+++ app/components/__tests__/MonthDropdown.js
@@ -1,10 +1,38 @@
 import React from 'react';
 import {shallow} from 'enzyme';
+
 import MonthDropdown from '../MonthDropdown';
 
 describe('MonthDropdown functionality', () => {
   test('MonthDropdown should render without issues', () => {
     const ddMonth = shallow(<MonthDropdown/>);
     expect(ddMonth.length).toEqual(1);
+  });
+
+  describe('Render the months in correct order', () => {
+    const monthDropdown = shallow(<MonthDropdown/>);
+    const selectList = monthDropdown.find('select');
+
+    test('There should be 12 months in total', () => {
+      expect(selectList.children().length).toBe(12);
+    });
+
+    test.each([
+      [0, 'January'],
+      [1, 'February'],
+      [2, 'March'],
+      [3, 'April'],
+      [4, 'May'],
+      [5, 'June'],
+      [6, 'July'],
+      [7, 'August'],
+      [8, 'September'],
+      [9, 'October'],
+      [10, 'November'],
+      [11, 'December'],
+    ])('item at index %i should be %p', (index, expectedMonthName) => {
+      expect(selectList.childAt(index).text()).toBe(expectedMonthName);
+      expect(selectList.childAt(index).prop('value')).toBe(index);
+    });
   });
 });
