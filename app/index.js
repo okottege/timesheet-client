869232485ee1b8a734e9d98bba11dc379be6ce3d
Oshan Kottege <oshan.kottege@gmail.com>
2019-02-09 19:37:40 +1300

    Removed the additional date picker component.

--- app/index.js
+++ app/index.js
@@ -7,13 +7,6 @@ import DatePicker from './components/DatePicker';
 import EmployeeForm from './components/employee/EmployeeForm';
 
 class App extends React.Component {
-  state = {
-    selectedDate: new Date()
-  };
-  onDateSelected = date => {
-    this.setState({selectedDate: date});
-  };
-
   render() {
     return (
       <div className="container-fluid">
@@ -21,10 +14,6 @@ class App extends React.Component {
           <strong>Simple Warning</strong>, best to take it easy.
         </Alert>
         <h1>Hello Mr Developer!</h1>
-        <div className="form-group">
-          <label>Date of birth</label>
-          <DatePicker className="form-control" selectedDate={this.state.selectedDate} onDateSelected={this.onDateSelected}/>
-        </div>
         <div>
           <EmployeeForm/>
         </div>
