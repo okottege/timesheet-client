2a4dece32d3b571ebb419651248486d41cd9e4b1
Oshan Kottege <oshan.kottege@myob.com>
2019-02-19 09:06:28 +1300

    Got the username and password fields updating the state as they change.

--- app/components/user/LoginForm.js
+++ app/components/user/LoginForm.js
@@ -1,27 +1,42 @@
 import React from 'react';
-import {Form} from 'react-bootstrap';
+import {Button, ButtonToolbar, Form} from 'react-bootstrap';
 
 import FormTextInput from '../common/FormTextInput';
 
-const onInputChanged = () => {};
+class LoginForm extends React.Component {
+  state = {
+    username: '',
+    password: ''
+  };
 
-function LoginForm() {
-  return (
-    <Form>
-      <FormTextInput
-        fieldId="txtUsername"
-        fieldName="username"
-        labelText="Username"
-        onInputChanged={onInputChanged} />
-      <FormTextInput
-        fieldId="txtPassword"
-        fieldName="password"
-        inputType="password"
-        labelText="Password"
-        onInputChanged={onInputChanged} />
-    </Form>
-  );
+  onInputChanged = e => {
+    const {field} = e;
+    this.setState(prevState => ({...prevState, [field]: e.target.value}));
+  };
+
+  render() {
+    return (
+      <Form>
+        <FormTextInput
+          fieldId="txtUsername"
+          fieldName="username"
+          labelText="Username"
+          value={this.state.username}
+          onInputChanged={e => this.onInputChanged({...e, field: 'username'})} />
+        <FormTextInput
+          fieldId="txtPassword"
+          fieldName="password"
+          inputType="password"
+          labelText="Password"
+          value={this.state.password}
+          onInputChanged={e => this.onInputChanged({...e, field: 'password'})} />
+
+        <ButtonToolbar>
+          <Button id="btnLogin" type="submit" variant="primary">Login</Button>
+        </ButtonToolbar>
+      </Form>
+    );
+  }
 }
-
 
 export default LoginForm;
--- app/components/user/__tests__/LoginForm.spec.js
+++ app/components/user/__tests__/LoginForm.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {shallow} from 'enzyme';
-import {Form} from 'react-bootstrap';
+import {Form, Button} from 'react-bootstrap';
 
 import LoginForm from '../LoginForm';
 import FormTextInput from '../../common/FormTextInput';
@@ -21,6 +21,24 @@ describe('Login form should', () => {
       expect(txtUsername.prop('inputType')).toBe(inputType);
       expect(txtUsername.prop('fieldName')).toBe(fieldName);
       expect(txtUsername.prop('labelText')).toBe(labelText);
+    });
+    test('a submit button with text "Login"', () => {
+      const loginButton = loginForm.find(Button);
+      expect(loginButton.exists()).toBe(true);
+      expect(loginButton.at(0).text()).toBe('Login');
+      expect(loginButton.prop('type')).toBe('submit');
+      expect(loginButton.prop('variant')).toBe('primary');
+    });
+  });
+
+  describe('capture field changes', () => {
+    test.each([
+      ['username', 'txtUsername', 'test.user'],
+      ['password', 'txtPassword', 'pass@word01']
+    ])('when %p field changes, its value is updated correctly', (field, fieldId, expValue) => {
+      const loginForm = shallow(<LoginForm />);
+      loginForm.find({fieldId}).prop('onInputChanged')({target: {value: expValue}, field});
+      expect(loginForm.find({fieldId}).prop('value')).toBe(expValue);
     });
   });
 });
